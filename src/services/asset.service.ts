6dca6f287d89f96a25b3cc84960f92c0e076bc93
LakeCombs <adesolaolamilekan2@gmail.com>
2022-11-07 22:09:42 +0100

    the main server lake combs edit

--- src/services/asset.service.ts
+++ src/services/asset.service.ts
@@ -1,6 +1,7 @@
-import GadgetModel from '@/models/assets/gadget.model';
-import vehicleModel from '@/models/assets/vehicle.model';
-import { GadgetType, VehicleType } from '@/types/asset.types';
+import GadgetModel from "@/models/assets/gadget.model";
+import vehicleModel from "@/models/assets/vehicle.model";
+import { GadgetType, VehicleType } from "@/types/asset.types";
+import { captureRejectionSymbol } from "events";
 
 /**
  * Asset service class
@@ -15,23 +16,45 @@ import { GadgetType, VehicleType } from '@/types/asset.types';
  * res.status(201).send(newAsset)
  */
 class asset_service {
-  /**
-   *
-   * @param {('cars' | 'bikes' | 'laptops' | 'phones')} category - Defines what category the asset belongs to
-   * @param {(GadgetType | VehicleType)} assetData - The data object to be used to create the new asset
-   * @returns Newly created asset could be a Gadget or Vehicle
-   */
-  async CREATE(
-    category: string,
-    assetData: VehicleType | GadgetType
-  ): Promise<VehicleType | GadgetType> {
-    const newAsset =
-      category === 'cars' || 'bikes'
-        ? await vehicleModel.create(assetData)
-        : await GadgetModel.create(assetData);
+	/**
+	 *
+	 * @param {('cars' | 'bikes' | 'laptops' | 'phones')} category - Defines what category the asset belongs to
+	 * @param {(GadgetType | VehicleType)} assetData - The data object to be used to create the new asset
+	 * @returns Newly created asset could be a Gadget or Vehicle
+	 */
+	async CREATE(
+		category: string,
+		assetData: VehicleType | GadgetType
+	): Promise<VehicleType | GadgetType> {
+		const newAsset =
+			category === "cars" || "bikes"
+				? await vehicleModel.create(assetData)
+				: await GadgetModel.create(assetData);
 
-    return newAsset;
-  }
+		return newAsset;
+	}
+
+	async GET_ALL_GADGET_OR_VEHICLE(type_of_asset: string): Promise<any> {
+		const allGadgetOrVehicle =
+			type_of_asset === "vehicle" || "gadget"
+				? await vehicleModel.find()
+				: await GadgetModel.find();
+
+		return allGadgetOrVehicle;
+	}
+
+	async UPDATE_GADGET_OR_VEHICLE_DATA(
+		id: string,
+		assetType: string,
+		update: VehicleType | GadgetType
+	): Promise<any> {
+		const updateAsset =
+			assetType === "vehicle" || "gadget"
+				? await vehicleModel.findByIdAndUpdate(id, { update }, { new: true })
+				: await GadgetModel.findByIdAndUpdate(id, { update }, { new: true });
+
+		return updateAsset;
+	}
 }
 
 export default new asset_service();
